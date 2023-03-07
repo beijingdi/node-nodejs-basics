74fbcbd765a3e2e2754040db30ee192c87799dff
Di Wu <dwu233@gmail.com>
2023-03-06 22:31:36 -0500

    create and copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,37 @@
+import fs from "fs/promises";
+import path from "path";
+
+const fileExists = async (path) => {
+    try{
+        await fs.access(path,fs.constants_F_OK);
+        console.log('file exists');
+        return true;
+    } catch(err){
+        console.log('file does not exists');
+        return false;
+    }
+}
+
 const copy = async () => {
+    try{
+        const folderExists = await fileExists('./src/fs/files',fs.constants_F_OK);
+        const copyFolderExists = await fileExists('./src/fs/files_copy',fs.constants_F_OK);
+        if (!folderExists && copyFolderExists){
+            throw new Error('FS operation failed');
+        }
+
+        await fs.mkdir('./src/fs/files_copy');
+        const files = await fs.readdir('./src/fs/files');
+        files.forEach(async(file)=>{
+            const sourcePath = path.join('./src/fs/files',file);
+            const destinationPath = path.join('./src/fs/files_copy',file);
+            await fs.copyFile(sourcePath,destinationPath);
+        })
+    } catch(err){
+        console.log(err);
+    }
     // Write your code here 
 };
 
 await copy();
+
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,31 @@
+import fs from "fs/promises";
+
+
+const fileExists = async (path) => {
+    try{
+        await fs.access(path,fs.constants_F_OK);
+        console.log('file exists');
+        return true;
+    } catch(err){
+        console.log('file does not exists');
+        return false;
+    }
+}
+
 const create = async () => {
-    // Write your code here 
+    console.log(process.cwd());
+    try{
+        const exist = await fileExists('./src/fs/files/fresh.txt');
+        if (exist) {
+            throw new Error('FS operation failed');
+        }
+        await fs.writeFile('./src/fs/files/fresh.txt', 'I am fresh and young');
+    }catch(err){
+        console.log(err);
+    }
 };
 
+
 await create();
+
+
